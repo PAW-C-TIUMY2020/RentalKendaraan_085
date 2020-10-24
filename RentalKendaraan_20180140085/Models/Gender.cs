f1b5f08f1d78eddd0627d0df0407c4a4f37b18e5
Mochammad Ifan Rizky Kurniawan <62989237+IfanRizky@users.noreply.github.com>
2020-10-24 11:40:01 +0700

    Add project files.

---
+++ RentalKendaraan_20180140085/Models/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan_20180140085.Models
+{
+    public partial class Gender
+    {
+        public int IdGender { get; set; }
+        public string NamaGender { get; set; }
+
+        public Customer Customer { get; set; }
+    }
+}
