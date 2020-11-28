41482472af0b582b9a03e66a034f291c765b54d6
Mochammad Ifan Rizky Kurniawan <62989237+IfanRizky@users.noreply.github.com>
2020-11-28 10:52:53 +0700

    add code in class FilesViewModel

--- RentalKendaraan_20180140085/Models/Upload/FilesViewModel.cs
+++ RentalKendaraan_20180140085/Models/Upload/FilesViewModel.cs
@@ -5,7 +5,14 @@ using System.Threading.Tasks;
 
 namespace RentalKendaraan_20180140085.Models.Upload
 {
+    public class FileDetails
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+
     public class FilesViewModel
     {
+        public List<FileDetails> Files { get; set; } = new List<FileDetails>();
     }
 }
